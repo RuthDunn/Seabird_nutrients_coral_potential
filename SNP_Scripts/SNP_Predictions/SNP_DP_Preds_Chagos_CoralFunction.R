aed852d0acb425050c0d088d9cccfb6640d1480d
Ruth Dunn <Ruth@HPspectrex3601.win.pet.hw.ac.uk>
2023-12-11 11:15:33 +0000

    Add in mid-veg scenario to coral reef functioning predictions

--- SNP_Scripts/SNP_Predictions/SNP_DP_Preds_Chagos_CoralFunction.R
+++ SNP_Scripts/SNP_Predictions/SNP_DP_Preds_Chagos_CoralFunction.R
@@ -25,11 +25,12 @@ load("SNP_ModelOutputs/Fish_Nitrogen_c.Rdata")
 pred.data <- read.csv("SNP_Data/Processed/Seabird_NutrientInput_Chagos_Predicted_priors.csv")
 
 # Calculate total nitrogen input (sum of the 3 species)
-pred.data <- pred.data[,c("Atoll_Island", "Species", "NitrogenInput.Current", "NitrogenInput.high.nn", "NitrogenInput.low.nn")]
+pred.data <- pred.data[,c("Atoll_Island", "Species", "NitrogenInput.Current",
+                          "NitrogenInput.high.nn", "NitrogenInput.low.nn", "NitrogenInput.mid.nn")]
 
 # Find total nitrogen input per Atoll_Island
 # (Under the different scenarios)
-pred.data <- aggregate(pred.data[,c(3:5)], by = list(pred.data$Atoll_Island), FUN = "sum")
+pred.data <- aggregate(pred.data[,c(3:6)], by = list(pred.data$Atoll_Island), FUN = "sum")
 
 # Remove DG
 pred.data <- pred.data[-1,]
@@ -55,6 +56,8 @@ fish.current <- as.data.frame(predict(fishbiomass.model.run.center, newdata = pr
 graz.current <- as.data.frame(predict(grazing.model.run.center, newdata = pred.data.current, allow_new_levels = TRUE))
 eros.current <- as.data.frame(predict(erosion.model.run.center, newdata = pred.data.current, allow_new_levels = TRUE))
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # Bad veg predictions:
 
 pred.data$logNitrogen <- log(pred.data$NitrogenInput.high.nn)
@@ -68,6 +71,8 @@ coral.bveg <- as.data.frame(predict(coralgrowth.model.run.center, newdata = pred
 fish.bveg <- as.data.frame(predict(fishbiomass.model.run.center, newdata = pred.data.bveg, allow_new_levels = TRUE))
 graz.bveg <- as.data.frame(predict(grazing.model.run.center, newdata = pred.data.bveg, allow_new_levels = TRUE))
 eros.bveg <- as.data.frame(predict(erosion.model.run.center, newdata = pred.data.bveg, allow_new_levels = TRUE))
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Good veg predictions:
 
@@ -83,13 +88,29 @@ fish.gveg <- as.data.frame(predict(fishbiomass.model.run.center, newdata = pred.
 graz.gveg <- as.data.frame(predict(grazing.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
 eros.gveg <- as.data.frame(predict(erosion.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Mid veg predictions:
+
+pred.data$logNitrogen <- log(pred.data$NitrogenInput.mid.nn)
+
+# Center logNitrogen data around 1.86
+pred.data$clogNitrogen <- pred.data$logNitrogen - 1.86
+
+pred.data.gveg <- pred.data[,c("Atoll_Island", "clogNitrogen")]
+
+coral.mveg <- as.data.frame(predict(coralgrowth.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
+fish.mveg <- as.data.frame(predict(fishbiomass.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
+graz.mveg <- as.data.frame(predict(grazing.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
+eros.mveg <- as.data.frame(predict(erosion.model.run.center, newdata = pred.data.gveg, allow_new_levels = TRUE))
+
 rm(coralgrowth.model.run.center, erosion.model.run.center, grazing.model.run.center, fishbiomass.model.run.center)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Combine into one dataframe
 
-pred.data <- pred.data[,-5]
+# pred.data <- pred.data[,-6]
 
 pred.data$coralgrowth.current <- coral.current$Estimate
 pred.data$coralgrowth.current.low <- coral.current$Q2.5
@@ -98,6 +119,10 @@ pred.data$coralgrowth.current.high <- coral.current$Q97.5
 pred.data$coralgrowth.bveg <- coral.bveg$Estimate
 pred.data$coralgrowth.bveg.low <- coral.bveg$Q2.5
 pred.data$coralgrowth.bveg.high <- coral.bveg$Q97.5
+
+pred.data$coralgrowth.mveg <- coral.mveg$Estimate
+pred.data$coralgrowth.mveg.low <- coral.mveg$Q2.5
+pred.data$coralgrowth.mveg.high <- coral.mveg$Q97.5
 
 pred.data$coralgrowth.gveg <- coral.gveg$Estimate
 pred.data$coralgrowth.gveg.low <- coral.gveg$Q2.5
@@ -113,6 +138,10 @@ pred.data$fishbiomass.bveg <- fish.bveg$Estimate
 pred.data$fishbiomass.bveg.low <- fish.bveg$Q2.5
 pred.data$fishbiomass.bveg.high <- fish.bveg$Q97.5
 
+pred.data$fishbiomass.mveg <- fish.mveg$Estimate
+pred.data$fishbiomass.mveg.low <- fish.mveg$Q2.5
+pred.data$fishbiomass.mveg.high <- fish.mveg$Q97.5
+
 pred.data$fishbiomass.gveg <- fish.gveg$Estimate
 pred.data$fishbiomass.gveg.low <- fish.gveg$Q2.5
 pred.data$fishbiomass.gveg.high <- fish.gveg$Q97.5
@@ -127,6 +156,10 @@ pred.data$grazing.bveg <- graz.bveg$Estimate
 pred.data$grazing.bveg.low <- graz.bveg$Q2.5
 pred.data$grazing.bveg.high <- graz.bveg$Q97.5
 
+pred.data$grazing.mveg <- graz.mveg$Estimate
+pred.data$grazing.mveg.low <- graz.mveg$Q2.5
+pred.data$grazing.mveg.high <- graz.mveg$Q97.5
+
 pred.data$grazing.gveg <- graz.gveg$Estimate
 pred.data$grazing.gveg.low <- graz.gveg$Q2.5
 pred.data$grazing.gveg.high <- graz.gveg$Q97.5
@@ -140,6 +173,10 @@ pred.data$erosion.current.high <- eros.current$Q97.5
 pred.data$erosion.bveg <- eros.bveg$Estimate
 pred.data$erosion.bveg.low <- eros.bveg$Q2.5
 pred.data$erosion.bveg.high <- eros.bveg$Q97.5
+
+pred.data$erosion.mveg <- eros.mveg$Estimate
+pred.data$erosion.mveg.low <- eros.mveg$Q2.5
+pred.data$erosion.mveg.high <- eros.mveg$Q97.5
 
 pred.data$erosion.gveg <- eros.gveg$Estimate
 pred.data$erosion.gveg.low <- eros.gveg$Q2.5
